99d986345a201d1cf8359b270aa99a59ee2a89af
Adam Lamson <adam.lamson@colorado.edu>
2019-11-01 12:20:33 -0600

    Added structure to project

---
+++ include/take_two/take_two.hpp
@@ -0,0 +1,12 @@
+#ifndef TAKE_TWO_H
+#define TAKE_TWO_H
+
+class TakeTwo {
+private:
+public:
+  // TakeTwo();
+  static float Add(float a, float b);
+  // virtual ~TakeTwo();
+};
+
+#endif /* TAKE_TWO_H */
---
+++ src/take_two.cpp
@@ -0,0 +1,10 @@
+/**
+ * @author      : adamlamson (adamlamson@LamsonMacbookPro)
+ * @file        : take_two
+ * @created     : Friday Nov 01, 2019 12:16:03 MDT
+ */
+
+#include <take_two/take_two.hpp>
+
+float TakeTwo::Add(float a, float b) { return a + b; }
+
